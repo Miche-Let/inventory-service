ed5b8427f090b220a88557ed88d0300bfae37b00
ji-circle <sp202401jw@gmail.com>
2026-04-26 18:13:38 +0900

    test: 헬스체크 API 명세 및 컨텍스트 로드 테스트 추가

---
+++ src/test/java/com/michelet/inventory/presentation/ProductControllerTest.java
@@ -0,0 +1,55 @@
+package com.michelet.inventory.presentation;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.michelet.inventory.application.ProductCommandService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(
+    controllers = ProductController.class,
+    excludeAutoConfiguration = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
+        RedisAutoConfiguration.class
+        // 만약 여전히 Redisson 에러가 나면 "org.redisson.spring.starter.RedissonAutoConfiguration" 추가 가능
+    }
+)
+@AutoConfigureRestDocs
+@ActiveProfiles("test")
+@Import(RestDocsConfig.class)
+public class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private ProductCommandService productCommandService;
+
+    @Test
+    void healthCheck() throws Exception {
+        // Given
+        given(productCommandService.checkHealth()).willReturn("Inventory Command Service is Healthy");
+
+        // When & Then
+        mockMvc.perform(get("/api/v1/admin/products/health")
+                .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andDo(document("{class-name}/{method-name}"));
+    }
+}
