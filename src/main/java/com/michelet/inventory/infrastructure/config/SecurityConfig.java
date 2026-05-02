08e8bd0e20bc12f0d40b0bd207ebaf8aa18c75e8
ji-circle <sp202401jw@gmail.com>
2026-05-02 22:03:32 +0900

    fix: 코드래빗 피드백 반영 - internal .permitAll으로 다시 복구

--- src/main/java/com/michelet/inventory/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/michelet/inventory/infrastructure/config/SecurityConfig.java
@@ -19,8 +19,7 @@ public class SecurityConfig {
             .httpBasic(AbstractHttpConfigurer::disable) // Basic 인증 비활성화
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/v1/products/**", "/api/v1/admin/products/**").permitAll()
-                // 내부 통신(internal)도 최소한의 인증을 요구하도록 permitAll()에서 authenticated()로 변경
-                .requestMatchers("/internal/**").authenticated() //TODO 내부 통신 완성되면 구체적 권한으로 수정
+                .requestMatchers("/internal/**").permitAll()
                 .anyRequest().authenticated()
             );
 
